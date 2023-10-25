21c8888edaf0989b11adedd8e5a22d2083a121c6
Muhamad Gempar Anzalas <anzalas.muhammad@gmail.com>
2023-10-25 19:11:12 +0700

    fix sendemail middleware

--- middleware/emailsend.go
+++ middleware/emailsend.go
@@ -1,43 +1,20 @@
 package middleware
 
 import (
-	"bytes"
 	"fmt"
-	"html/template"
 
-	"github.com/sirupsen/logrus"
 	"gopkg.in/gomail.v2"
 )
 
 func SendEmailPenngajuan(img string, to string, url string, name string) error {
-	var body bytes.Buffer
-	t, err := template.ParseFiles("C:/Users/LENOVO/go/src/findnseek/middleware/template.html")
-	if err != nil {
-		logrus.Error("Failed to parse template:", err)
-		return err
-	}
 
-	data := struct {
-		Name string
-		URL  string
-		Img  string
-	}{
-		Name: name,
-		URL:  url,
-		Img:  img,
-	}
-
-	if err := t.Execute(&body, data); err != nil {
-		logrus.Error("Failed to execute template:", err)
-		return err
-	}
 	m := gomail.NewMessage()
 	m.SetHeader("From", "gempar835@gmail.com")
 	m.SetHeader("To", to)
 	m.SetAddressHeader("Cc", "gempar835@gmail.com", "Dan")
 	m.SetHeader("Subject", "Find N Seek!")
-	m.SetBody("text/html", body.String())
-	// m.Attach("C:/Users/LENOVO/go/src/findnseek/middleware/Naruto.webp")
+	m.SetBody("text/html", fmt.Sprintf("<h2>Hei Apakah Ini Barang Miiikmu?</h2> <img src=%s>", img))
+	m.Attach(img)
 
 	d := gomail.NewDialer("smtp.gmail.com", 587, "gempar835@gmail.com", "xqrfdvnmscimxsgs")
 	if err := d.DialAndSend(m); err != nil {
