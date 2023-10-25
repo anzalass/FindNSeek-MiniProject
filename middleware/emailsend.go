448883a1435769293ae7250ffd997e536dcc76b4
anzalass <109114576+anzalass@users.noreply.github.com>
2023-10-25 18:48:29 +0700

    Update emailsend.go

--- middleware/emailsend.go
+++ middleware/emailsend.go
@@ -11,7 +11,7 @@ import (
 
 func SendEmailPenngajuan(img string, to string, url string, name string) error {
 	var body bytes.Buffer
-	t, err := template.ParseFiles("./template.html")
+	t, err := template.ParseFiles("ubuntu@ip-172-31-10-133:~/findnseek/FindNSeek-MiniProject/middleware/template.html")
 	if err != nil {
 		logrus.Error("Failed to parse template:", err)
 		return err
